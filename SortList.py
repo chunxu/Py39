00523ac7fda5ca8932ee2604ed71bd0dc3250f88
chunxu <41453752+chunxu@users.noreply.github.com>
2022-06-30 17:44:37 -0500

    Update SortList.py

--- SortList.py
+++ SortList.py
@@ -6,3 +6,11 @@
 # SortLst([4, 2, 1, 3, 4, 5]) returns [1, 2, 3, 4, 4, 5]
 # SortLst([1, 3, 5]) returns [1, 3, 5]
 
+def SortLst(mylist):
+    newlist = list()
+    while len(mylist)>0:
+        newitem = min(mylist)
+        newindex = mylist.index(newitem)
+        newlist.append(newitem)
+        mylist.pop(newindex)
+    return newlist
