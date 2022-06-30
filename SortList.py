da49c0e83e35b5e9ab9f7e0747b6cc5d9eaa1841
chunxu <41453752+chunxu@users.noreply.github.com>
2022-06-30 17:27:33 -0500

    Create SortList.py

---
+++ SortList.py
@@ -0,0 +1,8 @@
+# Sort a List
+# Description
+# Build a function SortLst which sorts a list. At this time you can just use the built-in function in Python.
+
+# Examples
+# SortLst([4, 2, 1, 3, 4, 5]) returns [1, 2, 3, 4, 4, 5]
+# SortLst([1, 3, 5]) returns [1, 3, 5]
+
