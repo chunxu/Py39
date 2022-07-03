081a51e0e64f2cf6d8453dd1e832db7ed5d685ee
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-03 09:58:32 -0500

    code

--- MaximumStockGain.py
+++ MaximumStockGain.py
@@ -6,3 +6,12 @@
 # MaxGain([3, 4, 1, 5, 7, 2]) returns 6 --Explanation: Buy stocks when the price is 1 and sell them when the price is 7.
 # MaxGain([5, 4, 3, 2, 1]) returns 0
 
+def MaxGain(mylist):
+    maxgain = 0
+    for i in range(0, len(mylist)):
+        j=i
+        while j<len(mylist):
+            if mylist[j] - mylist[i] > maxgain:
+                maxgain = mylist[j] - mylist[i]
+            j = j + 1
+    return maxgain
