f40eed3205cbe7654de992f29bf95b1ac7956574
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-03 09:58:19 -0500

    Create MaximumStockGain.py

---
+++ MaximumStockGain.py
@@ -0,0 +1,8 @@
+# Maximum Stock Gain
+# Description
+# Build a function MaxGain to find the maximum you can gain by buying and selling stocks. The strock prices represented as a list of numbers. You need to buy stocks before you sell them.
+
+# Examples
+# MaxGain([3, 4, 1, 5, 7, 2]) returns 6 --Explanation: Buy stocks when the price is 1 and sell them when the price is 7.
+# MaxGain([5, 4, 3, 2, 1]) returns 0
+
