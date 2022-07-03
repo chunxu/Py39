140bd627a545be8d850833fbe3719919297f3d0c
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-03 10:00:25 -0500

    Update SortListofLists.py

--- SortListofLists.py
+++ SortListofLists.py
@@ -5,3 +5,19 @@
 # Examples
 # SortLL([[1, 3, 5], [2, 4], [9, 7], [3]]) returns [[3], [2, 4], [1, 3, 5], [9, 7]]
 
+def SortLL(mylist):
+    newlist = list()
+    itemlist = list()
+    for i in range(0,len(mylist)):
+        itemlist.append(mylist[i][-1])
+    while len(itemlist)>0:
+        min_value = min(itemlist)
+        min_index = itemlist.index(min_value)
+        newlist.append(mylist[min_index])
+        itemlist.pop(min_index)
+        mylist.pop(min_index)
+    return newlist
+
+
+
+    
