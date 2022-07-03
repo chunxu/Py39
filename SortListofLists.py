63b58618c66ee8c572cc952864a6addb32ceb974
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-03 10:00:05 -0500

    description

--- SortListofLists.py
+++ SortListofLists.py
@@ -1,1 +1,7 @@
+# Sort List of Lists
+# Description
+# Build a function SortLL which sorts a list of lists by their last elements.
+
+# Examples
+# SortLL([[1, 3, 5], [2, 4], [9, 7], [3]]) returns [[3], [2, 4], [1, 3, 5], [9, 7]]
 
