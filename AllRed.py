42bf188d0629e31633e87cacb88aac49f810d78a
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-14 23:44:42 -0500

    description

--- AllRed.py
+++ AllRed.py
@@ -1,1 +1,8 @@
 #
+# Description
+# Build a function AllRed that takes a list of strings as input, returns a boolean indicating whether all the strings containing word "red".
+
+# Examples
+# AllRed(["red hat", "a pair of red shoes", "three red apples"]) returns True
+# AllRed(["red hat", "white shirt", "black eyes"]) returns False
+# AllRed([]) returns False
