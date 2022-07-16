f0cf5b8b1f64689b1e9f4bd2d82edcdb67d5f970
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-16 07:42:58 -0500

    Update RevStr.py

--- RevStr.py
+++ RevStr.py
@@ -4,3 +4,12 @@
 
 # Examples
 # RevStr("abcd") returns "dcba"
+
+def RevStr(mystring):
+    mylist=list(mystring)
+    newlist=list()
+    for i in range(0,len(mylist)):
+        newlist.append(mylist.pop())
+    newstring=""
+    newstring = newstring.join(newlist)
+    return newstring
