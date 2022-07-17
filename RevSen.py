5d324403e37caeff9a3da207d48fde93a8eb7540
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-17 08:39:50 -0500

    Update RevSen.py

--- RevSen.py
+++ RevSen.py
@@ -4,3 +4,10 @@
 # Examples
 # RevSen("we are all friends") returns "friends all are we”
 
+def RevSen(mysentence):
+    setencelist=mysentence.split()
+    newlist=list()
+    while len(setencelist)>0:
+        newlist.append(setencelist.pop())
+    return " ".join(newlist)
+
