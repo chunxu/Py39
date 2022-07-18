e83c0330c2f889229e938ee961b485e025f326bb
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-18 10:39:42 -0500

    Update RevWords.py

--- RevWords.py
+++ RevWords.py
@@ -4,3 +4,14 @@
 # Examples
 # RevWords("we are all friends") returns "ew era lla sdneirf"
 
+def RevWords(mysen):
+    mylist = mysen.split()
+    newstr =[]
+    for i in range (0, len(mylist)):
+        mystr = list(mylist[i])
+        newword=""
+        while(len(mystr)>0):
+            newstr.append(mystr.pop())
+        newstr.append(' ')
+    newstr.pop() #remove the last space
+    return "".join(newstr)
