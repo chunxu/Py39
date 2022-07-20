02e3347aee3b5f84d2f89b5120f7bc18606f9be9
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-20 00:24:30 -0500

    Update CapLett.py

--- CapLett.py
+++ CapLett.py
@@ -3,3 +3,12 @@
 
 # Examples
 # CapLett("we are all friends") returns "We Are All Friends"
+
+def CapLett(mysen):
+    mylist = mysen.split()
+    mystring = ""
+    for i in range(0,len(mylist)):
+        mylist[i] = mylist[i].capitalize() 
+    for i in range(0,len(mylist)):
+        mystring += (mylist[i]+" ") # not join
+    return mystring[:-1] # remove last space
