1de9039db9dca049edad8fed8e0b3d71604c6f96
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-21 11:12:38 -0500

    description

--- RmPunct.py
+++ RmPunct.py
@@ -1,1 +1,8 @@
+# Remove Punctuations
+# Description
+# Build a function RmPunct to remove punctuations from a string.
+
+# Examples
+# RmPunct("He said, that is great!!") returns "He said that is great"
+
 
