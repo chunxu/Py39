7a643669354e8c8dce503aaa1ed903639137740e
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-23 16:37:15 -0500

    description

---
+++ NGram.py
@@ -0,0 +1,10 @@
+# N Grams
+# Description
+# Build a function NGram which takes a string s and an integer n as inputs, returns a list of n grams of s.
+
+# Examples
+# NGram("techlent", 2) returns ["te", "ec", "ch", "hl", "le", "en", "nt"]
+# NGram("abc", 3) returns ["abc"]
+# NGram("abc", 4) returns [   ]
+# NGram("abc", 1) returns ["a", "b", "c"]
+
