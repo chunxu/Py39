c81fa5098a0e6970ae964b8c7d96d82aeed4c438
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-25 00:16:12 -0500

    Create isPalindrome.py

---
+++ isPalindrome.py
@@ -0,0 +1,11 @@
+# Palindrome
+# Description
+# Build a function isPalindrome which takes a string as an input and returns boolin value telling whether the string is a palindrome.
+
+# Examples
+# isPalindrome("abcba") returns True
+# isPalindrome("abba") returns True
+# isPalindrome("a") returns True
+# isPalindrome("") returns True
+# isPalindrome("abcd") returns False
+
