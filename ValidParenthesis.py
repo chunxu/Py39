e56f2c3b5894bd983df86f4761c7b5d918e09c63
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-27 16:12:41 -0500

    Create ValidParenthesis.py

---
+++ ValidParenthesis.py
@@ -0,0 +1,10 @@
+# Valid Parenthesis
+# Description
+# Build a function ValidParenthesis to determine the parenthesis in a string are valid.
+
+# Examples
+# ValidParenthesis("((()))()") returns True
+# ValidParenthesis("()()()") returns True
+# ValidParenthesis("()()()(") returns False
+# ValidParenthesis("((())))") returns False
+
