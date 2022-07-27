23fc947d978b40cf7ee9b50219ff1f89bca917d8
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-26 21:54:53 -0500

    Create LettPalind.py

---
+++ LettPalind.py
@@ -0,0 +1,9 @@
+# Letter Palindrome
+# Description
+# Build a function LettPalind to determine a string is a palindrome or not. We only consider letters this time.
+
+# Examples
+# LettPalind("909@,.") returns True
+# LettPalind("He is Sieh!") returns True
+# LettPalind("His name is Sieh.") returns False
+
