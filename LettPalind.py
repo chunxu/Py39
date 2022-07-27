469b33410f59d0c91fcbfa2c28088e94312b736f
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-26 21:55:10 -0500

    Update LettPalind.py

--- LettPalind.py
+++ LettPalind.py
@@ -7,3 +7,17 @@
 # LettPalind("He is Sieh!") returns True
 # LettPalind("His name is Sieh.") returns False
 
+def LettPalind(s):
+    mystring ="".join(ch for ch in s if ch.isalnum())
+    
+    i=0
+    j=True
+    while i<len(mystring)//2:
+        if mystring[i].lower() == mystring[-1-i].lower():
+            i+=1
+            continue
+        else:
+            j=False
+            break
+    return j
+
