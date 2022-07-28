b7dc6ed58a852bf753f1c937f27fa539297f04fb
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-28 09:30:55 -0500

    Create kvSwitch.py

---
+++ kvSwitch.py
@@ -0,0 +1,7 @@
+# Key Value Switch
+# Description
+# Build a function kvSwitch to switch the keys and values of a dictionary.
+
+# Examples
+# kvSwitch({"a": "1", "b": "2", "c": "3"}) returns {"1": "a", "2": "b", "3": “c"}
+
