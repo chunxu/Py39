34924802fb51afb7d173106f6f3f991f058c567b
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-29 11:13:07 -0500

    Update RankK.py

--- RankK.py
+++ RankK.py
@@ -5,3 +5,7 @@
 # Examples
 # RankK({"a": 7, "b": 5, "c": 9}) returns ["b", "a", "c"]
 
+def RankK(d):
+    mydic= {k:v for k, v in sorted(d.items(), key=lambda item: item[1])}
+    return list(mydic.keys())
+    
