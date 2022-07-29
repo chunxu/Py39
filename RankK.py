d0cb1408193cb79071320e5efd8a4fe35247e9bf
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-29 11:12:42 -0500

    Create RankK.py

---
+++ RankK.py
@@ -0,0 +1,7 @@
+# Rank Keys by Values
+# Description
+# Build a function RankK which takes a dictionary as input returns a list of keys which sorted by their values.
+
+# Examples
+# RankK({"a": 7, "b": 5, "c": 9}) returns ["b", "a", "c"]
+
