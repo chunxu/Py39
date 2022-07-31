4960d9fac7803864272a2d0f35a7175c258c9de2
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-31 08:01:22 -0500

    Create UniqueOnly.py

---
+++ UniqueOnly.py
@@ -0,0 +1,8 @@
+# Unique Only
+# Description
+# Build a function UniqueOnly to detect whether a string only contains unique characters.
+
+# Examples
+# UniqueOnly("techlent") returns False
+# UniqueOnly("abcde") returns True
+
