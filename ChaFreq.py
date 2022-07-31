b2d5d933a35f39f262013040b717e4114d96c86d
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-30 20:28:41 -0500

    Update ChaFreq.py

--- ChaFreq.py
+++ ChaFreq.py
@@ -4,3 +4,15 @@
 
 # Examples
 # ChaFreq("techlent") returns {"c": 1, "e": 2, "h": 1, "l": 1, "n": 1, "t": 2}
+
+def ChaFreq(s):
+    cha_list=[]
+    #fre_list=[]
+    mydic = {}
+    for char in s:
+        if char not in cha_list:
+            cha_list.append(char)
+            mydic[char] = 1
+        else:
+            mydic[char] +=1
+    return mydic
