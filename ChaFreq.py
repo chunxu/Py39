891c3bb0262833f1613fd06bc3b7c714cedaa232
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-30 20:28:24 -0500

    Create ChaFreq.py

---
+++ ChaFreq.py
@@ -0,0 +1,6 @@
+# Character Frequency
+# Description
+# Build a function ChaFreq to calculate the frequencies of characters in a string. Store the results in a dictionary.
+
+# Examples
+# ChaFreq("techlent") returns {"c": 1, "e": 2, "h": 1, "l": 1, "n": 1, "t": 2}
