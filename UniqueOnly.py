24058fb9445f29b4cd3c1b32531090bfb4de9141
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-31 08:01:39 -0500

    Update UniqueOnly.py

--- UniqueOnly.py
+++ UniqueOnly.py
@@ -6,3 +6,15 @@
 # UniqueOnly("techlent") returns False
 # UniqueOnly("abcde") returns True
 
+def UniqueOnly(s):
+    i = 0
+    mylist = []
+    value=True
+    while i<len(s):
+        if s[i] not in mylist:
+            mylist.append(s[i])
+            i +=1
+        else:
+            value = False
+            break
+    return value
