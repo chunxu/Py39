e1d7ccd91ca644e275e549277901dfab1911fd2c
chunxu <41453752+chunxu@users.noreply.github.com>
2022-08-01 13:34:53 -0500

    Create SamePattern.py

---
+++ SamePattern.py
@@ -0,0 +1,11 @@
+# Same Pattern
+# Description
+# Build a function SamePattern takes two strings as inputs and determines whether they are in the same pattern.
+
+# Examples
+# SamePattern("egg", "zoo") returns True
+# SamePattern("abb", "ab") returns False
+# SamePattern("hello", "agree") returns False
+# SamePattern("aaa", "abc") returns False
+# SamePattern("abc", "aaa") returns False
+
