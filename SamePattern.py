d02d5de8b5fbedb1f8db71185c22ca4219214c10
chunxu <41453752+chunxu@users.noreply.github.com>
2022-08-01 13:35:09 -0500

    Update SamePattern.py

--- SamePattern.py
+++ SamePattern.py
@@ -9,3 +9,29 @@
 # SamePattern("aaa", "abc") returns False
 # SamePattern("abc", "aaa") returns False
 
+def SamePattern(str1, str2):
+    s1=list(str1)
+    s2=list(str2)
+    value=True
+    if len(s1) != len(s2):
+        value=False
+    else:
+        i=0
+        while i<(len(s1)-1):
+            a = ord(s1[i]) - ord(s1[i+1])
+            b = ord(s2[i]) - ord(s2[i+1])
+            if a==0:
+                if b==0:
+                    i+=1
+                    continue
+                else:
+                    value=False
+                    break
+            else:
+                if b==0:
+                    value=False
+                    break
+                else:
+                    i+=1
+                    continue
+    return value
