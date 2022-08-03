8390b258f3bc108930166f31c541fadb30991242
chunxu <41453752+chunxu@users.noreply.github.com>
2022-08-02 22:15:58 -0500

    Update anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -6,3 +6,22 @@
 # anagrams("abc", "cba") returns True
 # anagrams("ab", "abc") returns False
 # anagrams("abccba", "aabbbc") returns False
+
+def anagrams(str1, str2):
+    #s1=list(str1)
+    #s2=list(str2)
+    value=True
+    if len(str1) != len(str2):
+        value=False
+    else:
+        i=0
+        while i<len(str1):
+            if str1[i] != str2[-1-i]:
+                value = False
+                break 
+            else:
+                i+=1
+        if i == len(str1):
+            value = True
+    return value
+        
