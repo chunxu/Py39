4f3e7d5d642fefbad526e31f2f68539f6911113c
chunxu <41453752+chunxu@users.noreply.github.com>
2022-08-02 22:12:37 -0500

    Create anagrams.py

---
+++ anagrams.py
@@ -0,0 +1,8 @@
+# Anagrams
+# Description
+# Build a function anagrams to determine whether two given strings are anagrams.
+
+# Examples
+# anagrams("abc", "cba") returns True
+# anagrams("ab", "abc") returns False
+# anagrams("abccba", "aabbbc") returns False
