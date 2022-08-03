ccb837fb54300fce75b987159f3434539c3efe40
chunxu <41453752+chunxu@users.noreply.github.com>
2022-08-03 14:13:08 -0500

    Create TargetSum.py

---
+++ TargetSum.py
@@ -0,0 +1,9 @@
+# Target Sum
+# Description
+# Build a function TargetSum which takes a list and a target number as inputs. If there are two numbers in the list whose sum equals to the target number, the function returns the indice of the two numbers in list, otherwise returns -1.
+
+# Examples
+# TargetSum([1, 2, 5, 9, 8, 11], 6) returns [0, 2] (1+5=6)
+# TargetSum([1, 2, 5, 9, 8, 11], 11) returns [1, 3]
+# TargetSum([1, 2, 5, 9, 8, 11], 5) returns -1
+# TargetSum([1, 2, 5, 9, 8, 11], 14) returns [2, 3]
